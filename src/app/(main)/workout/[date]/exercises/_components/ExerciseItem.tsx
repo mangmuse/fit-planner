30abef64f2d3c0d14776574197bd9f8e89c3ef2f
mangmuse <mangmangmuse@gmail.com>
2025-02-18 09:13:49 +0900

    test: add unit test for ExerciseList component

--- src/app/(main)/workout/[date]/exercises/_components/ExerciseItem.tsx
+++ src/app/(main)/workout/[date]/exercises/_components/ExerciseItem.tsx
@@ -50,9 +50,8 @@ const ExerciseItem = ({
   };
   return (
     <li
+      aria-labelledby="name"
       onClick={handleClick}
-      data-name={name}
-      data-bookmark={isBookmarked}
       className="px-3 flex justify-between w-full h-[51px] rounded-lg bg-bg-surface"
     >
       <div className="flex items-center gap-5">
@@ -61,12 +60,7 @@ const ExerciseItem = ({
         </div>
         <span className={clsx({ "text-primary": isSelected })}>{name}</span>
       </div>
-      <div
-        role="button"
-        data-testid="bookmark-toggle"
-        className="self-center"
-        onClick={handleToggleBookmark}
-      >
+      <div role="button" className="self-center" onClick={handleToggleBookmark}>
         <Image
           src={isBookmarked ? filledFavoriteIcon : favoriteIcon}
           alt={isBookmarked ? "북마크 해제" : "북마크"}
