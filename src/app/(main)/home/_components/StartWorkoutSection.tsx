860703265b687266b115d5673a5cd60aff7b8e56
mangmuse <mangmangmuse@gmail.com>
2025-06-12 20:55:23 +0900

    style: improve home page UI

--- src/app/(main)/home/_components/StartWorkoutSection.tsx
+++ src/app/(main)/home/_components/StartWorkoutSection.tsx
@@ -5,11 +5,11 @@ const StartWorkoutSection = () => {
   const formattedDate = getFormattedDate();
   const today = getFormattedDateYMD();
   return (
-    <section className="flex flex-col justify-between p-[18px] mb-[27px] w-full h-32 rounded-[20px] bg-bg-surface ">
-      <p className="text-lg">{formattedDate}</p>
+    <section className="flex flex-col justify-between p-4 mb-6 w-full h-28 rounded-[20px] bg-bg-surface shadow-sm">
+      <p className="text-base text-text-muted">{formattedDate}</p>
       <Link
         href={`/workout/${today}`}
-        className="flex text-lg justify-center items-center w-full font-semibold h-[49px] rounded-2xl bg-primary text-text-black"
+        className="flex text-base justify-center items-center w-full font-semibold h-11 rounded-xl bg-primary text-text-black hover:bg-opacity-90 transition-colors"
       >
         오늘의 운동 시작하기
       </Link>
