503af3582b2f0710f9834ee17c5796c26f2e9bb4
mangmuse <mangmangmuse@gmail.com>
2025-06-16 18:30:37 +0900

    chore: cleanup unused dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
 import type { NextConfig } from "next";
-import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
-
-const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
@@ -16,4 +13,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withVanillaExtract(nextConfig);
+export default nextConfig;
