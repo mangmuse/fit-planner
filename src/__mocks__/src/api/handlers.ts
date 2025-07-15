55409912a16bec9efe6c07bdbd2351089d80d7e8
mangmuse <mangmangmuse@gmail.com>
2025-07-15 12:25:50 +0900

    refactor: remove unused zustand store and dto

--- src/__mocks__/src/api/handlers.ts
+++ src/__mocks__/src/api/handlers.ts
@@ -1,9 +1,7 @@
 import { BASE_URL } from "@/constants";
 import { http, HttpResponse } from "msw";
 import dayjs from "dayjs";
-import { PatchBookmarkInput } from "@/types/dto/exercise.dto";
 import {
-  mockExercise,
   mockFetchExercisesResponse,
   mockpostExercisesResponse,
 } from "@/__mocks__/exercise.mock";
@@ -11,8 +9,6 @@ import {
   FetchExercisesResponse,
   SyncExercisesToServerResponse,
 } from "@/api/exercise.api";
-import { FetchWorkoutsResponse } from "@/api/workout.api";
-import { mockFetchWorkoutsResponse } from "@/__mocks__/workout.mock";
 
 const mockSession = {
   expires: dayjs().add(2, "day").toISOString(),
@@ -24,18 +20,6 @@ const mockSession = {
       "https://lh3.googleusercontent.com/a/ACg8ocJ4pjHLq0-tllg2mv3WjpP_EbpCYn1sUzhG0UQ0ojGDJ9JGHA=s96-c",
   },
 };
-
-// jest.mock("next-auth/react", () => {
-//   const originalModule = jest.requireActual("next-auth/react");
-
-//   return {
-//     __esModule: true, // ES 모듈 호환을 위함
-//     ...originalModule, // 기존 모듈의 내보내기를 보존하면서 리턴한다.
-//     useSession: jest.fn(() => {
-//       return { data: mockSession, status: "authenticated" };
-//     }),
-//   };
-// });
 
 export const handlers = [
   // ======== Exercise API ========
@@ -50,25 +34,14 @@ export const handlers = [
     return HttpResponse.json(res, { status: 201 });
   }),
 
-  // ======== Workout API ========
-  // http.get(`${BASE_URL}/api/workout`, () => {
-  //   const res: FetchWorkoutsResponse = mockFetchWorkoutsResponse;
-  //   return HttpResponse.json(res, { status: 200 });
-  // }),
-
-  // ========
-
   http.get(`/api/auth/session`, ({ request }) => {
     const isAuthenticated =
       request.headers.get("authorization") === "Bearer token";
 
-    // if (isAuthenticated) {
-    return new Response(JSON.stringify(mockSession), { status: 200 });
-    // }
+    if (isAuthenticated) {
+      return new Response(JSON.stringify(mockSession), { status: 200 });
+    }
 
-    // return new Response(JSON.stringify({}), { status: 401 });
+    return new Response(JSON.stringify({}), { status: 401 });
   }),
-  // http.post(`${BASE_URL}/api/auth/_log`, ({ request }) => {
-  //   return new Response(null, { status: 200 });
-  // }),
 ];
